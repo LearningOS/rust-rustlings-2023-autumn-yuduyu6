9c83051b3b06dea6453414da130a78c737d58a26
yuduyu6 <1375951987@qq.com>
2023-10-19 00:18:42 +0800

    update

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -3,18 +3,18 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
-    fn get_secret_recipe() -> String {
+         fn get_secret_recipe() -> String {
         String::from("Ginger")
         
     }
 
-    fn make_sausage() {
-        let secret_recipe=get_secret_recipe();
-        println!("{}", secret_recipe);
+     pub fn make_sausage() {
+        
+        println!("sausage!");
     }
 }
 
